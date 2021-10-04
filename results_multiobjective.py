aea32fd215f74c8a6d346539de6f22423953d8f8
FedePeralta <peralta.s.federico@gmail.com>
2021-10-04 22:07:51 +0200

    Version 1.0

--- results_multiobjective.py
+++ results_multiobjective.py
@@ -8,7 +8,8 @@ import mpl_toolkits.mplot3d.art3d
 '''
     Indicate the number of ships  using:
         number_of_ships = 1
-    Works with 1, 2 and 3 ships.
+    Works with 1, 2 and 3 ships for 3 objectives.
+    Works with 1 ship for 4 objectives. (
         
     For a functional resulting figure, one module of the matplotlib library (mpl_toolkits.mplot3d.art3d)
     needs to be modified. More information on https://github.com/FedePeralta/EMOPP
@@ -16,7 +17,15 @@ import mpl_toolkits.mplot3d.art3d
     Run the script and select any Pareto point in the Objective Space (1st sub-figure), the selected point will graph
     its individual representation in the second sub-figure (Search Space). 
 '''
-number_of_ships = 3
+number_of_ships = 1  # 1, 2 or 3
+
+# include_distance = False  # False for plotting the results with 3 objectives
+include_distance = True  # True for plotting the results with 4 objectives (including distance)
+
+if include_distance and number_of_ships != 1:
+    raise ValueError("Results for 4 objectives were only obtained with 1 ship")
+elif not include_distance and not (0 < number_of_ships < 4):
+    raise ValueError("Number of ships must be betwen 1 and 3")
 
 
 def get_clean(_file):
@@ -25,9 +34,11 @@ def get_clean(_file):
     return np.ma.array(_file, mask=np.isnan(_file))
 
 
-f3 = pd.read_csv(f"Results/fitness_multi_{number_of_ships}ships.txt", index_col=0)
+dist = '_distance' if include_distance else ''
+f3 = pd.read_csv(f"Results/fitness_multi_{number_of_ships}ships{dist}.txt", index_col=0)
+print(f3)
 ind = []
-with open(f"Results/ind_multi_{number_of_ships}ships.txt") as f:
+with open(f"Results/ind_multi_{number_of_ships}ships{dist}.txt") as f:
     line = f.readline()
     while line != "":
         full_ind = np.array([int(i) for i in line[line.find(",") + 2:-2].split(',')])
@@ -47,12 +58,19 @@ obj_space.set_xlabel('$Shekel$', fontsize=15)
 obj_space.set_ylabel('$Himmelblau$', fontsize=15)
 obj_space.set_zlabel('$Rosenbrock$', fontsize=15)
 
-obj_space.scatter(np.zeros_like(f3["hb"]), f3["hb"], f3["rb"], c=np.zeros_like(f3["sh"]), cmap='binary_r', alpha=0.05)
-obj_space.scatter(f3["sh"], np.zeros_like(f3["hb"]), f3["rb"], c=np.zeros_like(f3["sh"]), cmap='binary_r', alpha=0.05)
-obj_space.scatter(f3["sh"], f3["hb"], np.zeros_like(f3["hb"]), c=np.zeros_like(f3["sh"]), cmap='binary_r', alpha=0.05)
+if include_distance:
+    markers = obj_space.scatter(f3["sh"], f3["hb"], f3["rb"], c=np.arange(0, len(f3["sh"])), cmap='jet', picker=True,
+                                pickradius=2, s=f3["di"], alpha=0.2)
+else:
+    obj_space.scatter(np.zeros_like(f3["hb"]), f3["hb"], f3["rb"], c=np.zeros_like(f3["sh"]), cmap='binary_r',
+                      alpha=0.05)
+    obj_space.scatter(f3["sh"], np.zeros_like(f3["hb"]), f3["rb"], c=np.zeros_like(f3["sh"]), cmap='binary_r',
+                      alpha=0.05)
+    obj_space.scatter(f3["sh"], f3["hb"], np.zeros_like(f3["hb"]), c=np.zeros_like(f3["sh"]), cmap='binary_r',
+                      alpha=0.05)
+    markers = obj_space.scatter(f3["sh"], f3["hb"], f3["rb"], c=np.arange(0, len(f3["sh"])), cmap='jet', picker=True,
+                                pickradius=2)
 
-markers = obj_space.scatter(f3["sh"], f3["hb"], f3["rb"], c=np.arange(0, len(f3["sh"])), cmap='jet', picker=True,
-                            pickradius=2)
 index = 0
 
 selected = [obj_space.plot(f3["sh"][index], f3["hb"][index], f3["rb"][index], 'Xk', zorder=600000,
@@ -65,7 +83,8 @@ selected = [obj_space.plot(f3["sh"][index], f3["hb"][index], f3["rb"][index], 'X
                            'k--', alpha=0.5, linewidth=2),
             obj_space.plot([f3["sh"][index], f3["sh"][index]], [0, f3["hb"][index]],
                            [f3["rb"][index], f3["rb"][index]],
-                           'k--', alpha=0.5, linewidth=2)]
+                           'k--', alpha=0.5, linewidth=2)
+            ]
 
 obj_space.legend()
 
